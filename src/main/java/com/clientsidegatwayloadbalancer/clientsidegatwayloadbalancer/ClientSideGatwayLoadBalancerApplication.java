983ee1809a9fa03017c8d0b51386e138c06531f0
Viraj Patil <virajspatil1998@gmail.com>
2024-04-03 17:05:33 +0530

    Client side load balancing using eureka client for productService and userService

---
+++ src/main/java/com/clientsidegatwayloadbalancer/clientsidegatwayloadbalancer/ClientSideGatwayLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.clientsidegatwayloadbalancer.clientsidegatwayloadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSideGatwayLoadBalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientSideGatwayLoadBalancerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clientsidegatwayloadbalancer/clientsidegatwayloadbalancer/ClientSideGatwayLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientsidegatwayloadbalancer.clientsidegatwayloadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSideGatwayLoadBalancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
